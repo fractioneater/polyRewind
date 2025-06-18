07d7cf8fec15ef106add67e8b9ff9822cdcc891c
fractioneater <fractioneater@gmail.com>
2025-06-18 18:10:13 -0400

    Still no luck

--- 0.1.0/mod.js
+++ 0.1.0/mod.js
@@ -1,19 +1,20 @@
-import { PolyMod } from "https://pml.crjakob.com/polytrackmods/PolyModLoader/0.5.0/PolyModLoader.js";
+import { PolyMod, MixinType } from "https://pml.crjakob.com/polytrackmods/PolyModLoader/0.5.0/PolyModLoader.js";
 
 class PolyRewind extends PolyMod {
 	init = (pml) => {
 		this.pml = pml;
 		console.log("PolyRewind says hi!");
 		this.pml.registerBindCategory("PolyRewind Mod");
-		this.pml.registerKeybind("Store", "storeFrame", "keydown", "KeyB", null, (e) => { /* If in game... */ this.storeFrame(e); });
-		this.pml.registerKeybind("Load", "loadFrame", "keydown", "KeyF", null, (e) => { /* If in game... */ this.loadFrame(e); });
-		
+		this.pml.registerKeybind("Store", "storeFrame", "keydown", "KeyB", null, (e) => { this.storeFrame(e); });
+		this.pml.registerKeybind("Load", "loadFrame", "keydown", "KeyF", null, (e) => { this.loadFrame(e); });
+
+		this.stores = [];
+
 		this.car = null;
-		this.spectator = null;
-		polyModLoader.registerFuncMixin("pP", MixinType.INSERT, `yP(this, eP, "f").setColors(n.carColors),`, `ActivePolyModLoader.getMod("${this.modID}").car = yP(this, eP, "f"),`);
-		polyModLoader.registerClassMixin("s_.prototype", "addToggleListener", MixinType.INSERT, `a_(this, QT, "f").push(e)`, `,ActivePolyModLoader.getMod("${this.modID}").spectator = this;`);
+		pml.registerFuncMixin("pP", MixinType.INSERT, `yP(this, eP, "f").setColors(n.carColors),`, `ActivePolyModLoader.getMod("${this.modID}").car = yP(this, eP, "f"),`);
+
 		// To call an update on interval script (for a real rewind).
-		// polyModLoader.registerFuncMixin("polyInitFunction", MixinType.INSERT, `y.setAnimationLoop((function(e) {`, `ActivePolyModLoader.getMod("pdip").update();`);
+		// pml.registerFuncMixin("polyInitFunction", MixinType.INSERT, `y.setAnimationLoop((function(e) {`, `ActivePolyModLoader.getMod("${this.modID}").update();`);
 	}
 
 	update = () => {
@@ -22,12 +23,22 @@ class PolyRewind extends PolyMod {
 
 	storeFrame = (e) => {
 		// Hmmm.
-		console.log(this.car);
+		if (this.car) {
+			window.car = this.car; //- REMOVE
+			const pos = this.car.getPosition();
+			const rotation = this.car.getQuaternion();
+			const speed = this.car.getSpeedKmh();
+			const time = this.car.getTime();
+			this.stores.push({ pos, rotation, speed, time });
+			console.log(this.stores[this.stores.length - 1]);
+		}
 	}
 
 	loadFrame = (e) => {
-		// Hmmm.
-		console.log(this.car);
+		if (this.car) {
+			console.log(this.stores);
+			const target = this.stores[this.stores.length - 1];
+		}
 	}
 }
 
