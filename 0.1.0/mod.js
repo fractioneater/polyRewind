35636fa524acd38f5ce6398f7d2313b022b792f0
fractioneater <fractioneater@gmail.com>
2025-06-17 21:07:15 -0400

    Untested mixins for car and spectator info

--- 0.1.0/mod.js
+++ 0.1.0/mod.js
@@ -7,16 +7,27 @@ class PolyRewind extends PolyMod {
 		this.pml.registerBindCategory("PolyRewind Mod");
 		this.pml.registerKeybind("Store", "storeFrame", "keydown", "KeyB", null, (e) => { /* If in game... */ this.storeFrame(e); });
 		this.pml.registerKeybind("Load", "loadFrame", "keydown", "KeyF", null, (e) => { /* If in game... */ this.loadFrame(e); });
+		
+		this.car = null;
+		this.spectator = null;
+		polyModLoader.registerFuncMixin("pP", MixinType.INSERT, `yP(this, eP, "f").setColors(n.carColors),`, `ActivePolyModLoader.getMod("${this.modID}").car = yP(this, eP, "f"),`);
+		polyModLoader.registerClassMixin("s_.prototype", "addToggleListener", MixinType.INSERT, `a_(this, QT, "f").push(e)`, `,ActivePolyModLoader.getMod("${this.modID}").spectator = this;`);
+		// To call an update on interval script (for a real rewind).
+		// polyModLoader.registerFuncMixin("polyInitFunction", MixinType.INSERT, `y.setAnimationLoop((function(e) {`, `ActivePolyModLoader.getMod("pdip").update();`);
+	}
+
+	update = () => {
+		// Huh.
 	}
 
 	storeFrame = (e) => {
 		// Hmmm.
-		console.log(this.pml);
+		console.log(this.car);
 	}
 
 	loadFrame = (e) => {
 		// Hmmm.
-		console.log(this.pml);
+		console.log(this.car);
 	}
 }
 
